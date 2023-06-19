836d25172d3fe5b3ba4b27e309f3463dda74cc69
CarlosRomio <carlosromiobezerra@gmail.com>
2023-06-19 20:49:01 -0300

    recebendo e preparando os valores

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-aposta = input("informe um número da sua aposta: ")
-aposta1 = aposta1 + aposta
+numeros = []
+while True:
+    aposta = input("informe um número da sua aposta: ")
+    if len(aposta) == 1:
+        aposta = "0" + aposta
+    elif aposta in numeros:
+        aposta = ''
+        print("Número já inserido")
+    elif int(aposta) >= 61:
+        aposta = ""
+        print("Insira números menores que 60")
+    if aposta != "":
+        aposta = [aposta]
+        numeros += aposta
+    if len(numeros) ==  6:
+        break
